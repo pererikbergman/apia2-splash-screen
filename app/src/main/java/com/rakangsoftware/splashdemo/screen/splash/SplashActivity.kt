04c831f991dc99539dd82fc1c1f5c451edddd014
“Per-Erik <bergman@uncle.se>
2018-09-11 19:51:54 +0200

    Splash screen added.

---
+++ app/src/main/java/com/rakangsoftware/splashdemo/screen/splash/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.rakangsoftware.splashdemo.screen.splash
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
